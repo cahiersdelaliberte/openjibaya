325fc60cc1fc823efcc6a8b85653d834b1056717
sandcha <sandcha@users.noreply.github.com>
2017-12-21 20:11:03 +0100

    Add intl package

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ require("babel-register")
 
 // Force Intl to be polyfilled because NodeJS French date format does not use padding (1/1/2016)
 // whereas npm intl module does (01/01/2016). This leads to DOM mismatch between server and client in React.
-//global.Intl = require("intl")
-
-// Start the server app
-//require("./src/server.js")
+global.Intl = require("intl")
 
 
 import React from 'react';
