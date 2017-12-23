23a1332e8f3fb50610b4b8d6047a7ade55dd1202
sandcha <sandcha@users.noreply.github.com>
2017-12-23 23:56:03 +0100

    Lift state to communicate inputs to different components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,54 @@
 import React from 'react'
-import InputSection from './InputSection.jsx'
+import BasicInput from './BasicInput.jsx'
 import Results from './Results.jsx'
 import Affiliation from './Affiliation.jsx'
 import "../assets/css/main.css"
 import "../assets/css/reset.css"
 
 
-let smic_tnd = 338
+
+const SMIG_TND_2016 = 338
+const SMIG_TND_AN_2016 = SMIG_TND_2016 * 12
 
 
 export default class App extends React.Component {
 
   constructor(props){
     super(props)
+
     this.state = { 
-      // typeSalaireEntre: 'brut',
-      // salaireNetAPayer: smic_tnd,
-      results: {
-        salaire_imposable: 17710,
-        salaire_net_a_payer: 14700
-      }
+    	// typeSalaireEntre: 'brut',
+    	// salaireNetAPayer: smic_tnd,
+		typeEmploye: "fonctionnaire",
+		salaire: SMIG_TND_AN_2016,
+		periodeSalaire: "an",
+		statutFamilial: "celibataire",
+		nbEnfants: 0,
+
+		results: {
+			salaire_imposable: 17710,
+			salaire_net_a_payer: 14700
+		}
     }
+    this.handleChange = this.handleChange.bind(this)
   }
+
+  	//see handleChange argument struct
+	handleChange(partialState){
+		this.setState(partialState)
+	}
 
   render() {
     console.log("App state.results: " + this.state.results.salaire_imposable + " " + this.state.results.salaire_net_a_payer)
     var simulatorName = "OpenJibaya"
     var inputTouched = false
     var inputChanged = false
+    //inputTouched={ inputTouched } inputChanged={ inputChanged } />
 
     return (
       <div className="widget">
-        <InputSection inputTouched={ inputTouched } inputChanged={ inputChanged } />
-        <Results results={ this.state.results }/>
+        <BasicInput salaire={ this.state.salaire } onUserChange={ this.handleChange } />
+        <Results results={ this.state.results } />
         <Affiliation />
       </div>
     );
