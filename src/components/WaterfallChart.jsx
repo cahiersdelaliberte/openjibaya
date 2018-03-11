3d95b68c46736c224398daad4a786ec74842a79e
sandcha <sandcha@users.noreply.github.com>
2018-03-11 23:54:22 +0100

    Update waterfall input data list

--- src/components/WaterfallChart.jsx
+++ src/components/WaterfallChart.jsx
@@ -4,91 +4,105 @@ import Highcharts from 'highcharts'
 const ReactHighcharts = require("react-highcharts");
 require("highcharts/js/highcharts-more")(ReactHighcharts.Highcharts);
 
-
-function draw(salaireNetAPayer, impots, cotisations) {
-	
-	//Calculé : var salaireSuperBrut = 3000
-	//var cotisations = 500
-	//Calculé : var salaireDeBase = 2500
-	//var impots = 1000
-	//var salaireNetAPayer = 1500
-
+function draw(salaireNetAPayer, salaireImposable, salaireDeBasePrimes, salaireSuperBrut) {
 	var chartTitle = 'Impôts & Cotisations'
 
 	// creation du graphique
 	return {
+		chart: {
+			type: 'waterfall'
+		},
 
-	        chart: {
-	            type: 'waterfall'
-	        },
+		title: {
+			text: chartTitle
+		},
 
-	        title: {
-	            text: chartTitle
-	        },
+		xAxis: {
+			type: 'category'
+		},
 
-	        xAxis: {
-	            type: 'category'
-	        },
+		yAxis: {
+			title: {
+				text: 'TND'
+			}
+		},
 
-	        yAxis: {
-	            title: {
-	                text: 'TND'
-	            }
-	        },
+		legend: {
+			enabled: false
+		},
 
-	        legend: {
-	            enabled: false
-	        },
+		tooltip: {
+			pointFormat: '<b>{point.y:,.2f}</b> TND'
+		},
 
-	        tooltip: {
-	            pointFormat: '<b>{point.y:,.2f}</b> TND'
-	        },
+		series: [{
+			upColor: Highcharts.getOptions().colors[2],
+			color: Highcharts.getOptions().colors[3],
+			data: [{
+				name: 'Salaire Net A Payer',
+				y: salaireNetAPayer,
+				color: Highcharts.getOptions().colors[1]
+			}, {
+				name: 'Impôts',
+				y: salaireImposable - salaireNetAPayer
+			}, {
+				name: 'Salaire Imposable',
+				isSum: true,
+				color: Highcharts.getOptions().colors[1]
+			}, {
+				name: 'Cotisations Salarié',
+				y: salaireDeBasePrimes - salaireImposable
+			}, {
+				name: 'Salaire De Base & Primes',
+				isSum: true,
+				color: Highcharts.getOptions().colors[1]
+			}, {
+				name: 'Cotisations Employeur',
+				y: salaireSuperBrut - salaireDeBasePrimes
+			}, {
+				name: 'Salaire Super Brut',
+				isSum: true,
+				color: Highcharts.getOptions().colors[1]
+			}],
 
-	        series: [{
-	            upColor: Highcharts.getOptions().colors[2],
-	            color: Highcharts.getOptions().colors[3],
-	            data: [{
-	                name: 'Salaire Net A Payer',
-	                y: salaireNetAPayer,
-	                color: Highcharts.getOptions().colors[1]
-	            }, {
-	                name: 'Impôts',
-	                y: impots,
-	            }, {
-	                name: 'Salaire De Base',
-	                isIntermediateSum: true,
-	                color: Highcharts.getOptions().colors[1]
-	            }, {
-	                name: 'Cotisations',
-	                y: cotisations
-	            }, {
-	                name: 'Salaire Super Brut',
-	                isSum: true,
-	                color: Highcharts.getOptions().colors[1]
-	            }],
-	            dataLabels: {
-	                enabled: true,
-	                formatter: function () {
-	                    return Highcharts.numberFormat(this.y / 1000, 0, ',') + 'k';
-	                },
-	                style: {
-	                    fontWeight: 'bold'
-	                }
-	            },
-	            pointPadding: 0
-	        }]
-	    } //end return
+			dataLabels: {
+				enabled: true,
+				formatter: function () {
+					return Highcharts.numberFormat(this.y, 0, ',') + 'TND';
+				},
+				style: {
+					fontWeight: 'bold'
+				}
+			},
+			pointPadding: 0
+		}]
+	} //end return
 } //end function
 
 export default class WaterfallChart extends Component {
 	render() {
 		//let salaireNetAPayer = this.props.results.salaire_net_a_payer,
-			//impots = this.props.results.irpp,
-			//cotisations = this.props.results.cotisations_employeur + this.props.results.cotisations_salarie 
-	
+		//impots = this.props.results.irpp,
+		//cotisations = this.props.results.cotisations_employeur + this.props.results.cotisations_salarie 
+
+		console.log('WaterfallChart')
+		var salaireNetAPayer = this.props.results['salaire_net_a_payer']
+		var salaireImposable = salaireNetAPayer + 500
+		console.log(salaireImposable)
+		var salaireDeBasePrimes = salaireImposable + 500
+		console.log(salaireDeBasePrimes)
+		var salaireSuperBrut = salaireDeBasePrimes + 500
+		console.log(salaireSuperBrut)
+
 		return (
-				//<ReactHighcharts config = {draw(salaireNetAPayer, impots, cotisations)}></ReactHighcharts>
-				<ReactHighcharts config = {draw(1500, 1000, 500)}></ReactHighcharts>
+				<ReactHighcharts config = {
+					draw(
+						salaireNetAPayer, 
+						salaireImposable, 
+						salaireDeBasePrimes, 
+						salaireSuperBrut
+						)}>
+				</ReactHighcharts>
 				)
 	}
 }
